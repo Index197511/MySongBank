f05c83f099c3c612cebd3f4b42f491fbf8ea5cc4
Index197511 <hiro.saten@gmail.com>
2020-07-03 20:50:57 +0900

    add koin for di

---
+++ app/src/main/java/me/index197511/mysongbank/di/MySongBankApp.kt
@@ -0,0 +1,12 @@
+package me.index197511.mysongbank.di
+
+import android.app.Application
+
+@Suppress("UNUSED")
+class MySongBankApp : Application() {
+
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/me/index197511/mysongbank/ui/songlist/SongListFragment.kt
@@ -0,0 +1,2 @@
+package me.index197511.mysongbank.ui.songlist
+
