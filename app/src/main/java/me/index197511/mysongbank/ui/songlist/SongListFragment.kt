236f54e721839c22d2a43ff2538a6966326c56a5
Index197511 <hiro.saten@gmail.com>
2020-07-05 12:14:08 +0900

    add SongRepository

--- app/src/main/java/me/index197511/mysongbank/ui/songlist/SongListFragment.kt
+++ app/src/main/java/me/index197511/mysongbank/ui/songlist/SongListFragment.kt
@@ -1,2 +1,37 @@
 package me.index197511.mysongbank.ui.songlist
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import me.index197511.mysongbank.databinding.SongListFragmentBinding
+
+class SongListFragment : Fragment() {
+
+    private lateinit var viewModel: SongListViewModel
+    private lateinit var binding: SongListFragmentBinding
+    private val adapter = GroupAdapter<GroupieViewHolder>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = SongListFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        binding.recyclerViewSongList.adapter = adapter
+
+        binding.buttonAddNewSong.setOnClickListener {
+            Log.i("DebugPrint", "buttonAddNewSong clicked")
+        }
+    }
+
+}
--- app/src/main/java/me/index197511/mysongbank/ui/songlist/SongListViewModel.kt
+++ app/src/main/java/me/index197511/mysongbank/ui/songlist/SongListViewModel.kt
@@ -1,2 +1,7 @@
 package me.index197511.mysongbank.ui.songlist
 
+import androidx.lifecycle.ViewModel
+
+class SongListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
