8eee15fde4ce0c7d739756a8a29264e4acac3792
Index197511 <hiro.saten@gmail.com>
2020-07-12 16:34:02 +0900

    add InstrumentedTest for Dao and test for conversion between Entity and Model

---
+++ app/src/test/java/me/index197511/mysongbank/song/SongTest.kt
@@ -0,0 +1,42 @@
+package me.index197511.mysongbank.song
+
+import com.google.common.truth.Truth.assertThat
+import me.index197511.mysongbank.db.entity.SongEntity
+import me.index197511.mysongbank.db.entity.toEntity
+import me.index197511.mysongbank.model.Song
+import org.junit.Test
+
+class SongTest {
+    @Test
+    fun songToEntity() {
+        val songModel = Song(id = 0, name = "Name", singer = "Singer", key = 10, memo = "Memo")
+        val songEntity = songModel.toEntity()
+        assertThat(songEntity).isInstanceOf(SongEntity::class.java)
+        assertThat(songEntity).isEqualTo(
+            SongEntity(
+                id = 0,
+                name = "Name",
+                singer = "Singer",
+                key = 10,
+                memo = "Memo"
+            )
+        )
+    }
+
+    @Test
+    fun songToModel() {
+        val songEntity =
+            SongEntity(id = 0, name = "Name", singer = "Singer", key = 10, memo = "Memo")
+        val songModel = songEntity.toModel()
+        assertThat(songModel).isInstanceOf(Song::class.java)
+        assertThat(songModel).isEqualTo(
+            Song(
+                id = 0,
+                name = "Name",
+                singer = "Singer",
+                key = 10,
+                memo = "Memo"
+            )
+        )
+    }
+}
